656aa334b4814b8eda26cb5cf7b8f343ca3f29d5
Andreas Lind Petersen <andreas@one.com>
2013-10-08 13:50:32 +0200

    gitWrappedFs => fsPlusGit

--- test/FsPlusGit.js
+++ test/FsPlusGit.js
@@ -8,7 +8,7 @@ var expect = require('unexpected-sinon'),
 
 describe('FsPlusGit', function () {
     var pathToTestRepo = Path.resolve(__dirname, '..', 'node_modules', 'gitfakefs', 'test', 'testrepo.git'),
-        gitWrappedFs;
+        fsPlusGit;
 
     describe('applied to the built-in fs module', function () {
         var fs;
@@ -19,13 +19,13 @@ describe('FsPlusGit', function () {
                     sinon.spy(fs, propertyName);
                 }
             });
-            gitWrappedFs = new FsPlusGit(fs);
+            fsPlusGit = new FsPlusGit(fs);
         });
 
         ['stat', 'lstat'].forEach(function (methodName) {
             describe('#' + methodName + '()', function () {
                 it('should report /gitFakeFs/ as a directory', function (done) {
-                    gitWrappedFs.stat(Path.resolve(pathToTestRepo, 'gitFakeFs'), passError(done, function (stats) {
+                    fsPlusGit.stat(Path.resolve(pathToTestRepo, 'gitFakeFs'), passError(done, function (stats) {
                         expect(stats.isDirectory(), 'to be', true);
                         expect(stats.isFile(), 'to be', false);
                         done();
@@ -33,7 +33,7 @@ describe('FsPlusGit', function () {
                 });
 
                 it('should report /gitFakeFs/HEAD/ as a directory', function (done) {
-                    gitWrappedFs.stat(Path.resolve(pathToTestRepo, 'gitFakeFs', 'HEAD'), passError(done, function (stats) {
+                    fsPlusGit.stat(Path.resolve(pathToTestRepo, 'gitFakeFs', 'HEAD'), passError(done, function (stats) {
                         expect(stats.isDirectory(), 'to be', true);
                         expect(stats.isFile(), 'to be', false);
                         done();
@@ -41,7 +41,7 @@ describe('FsPlusGit', function () {
                 });
 
                 it('should report /gitFakeFs/HEAD/subdir/ as a directory', function (done) {
-                    gitWrappedFs.stat(Path.resolve(pathToTestRepo, 'gitFakeFs', 'HEAD', 'subdir'), passError(done, function (stats) {
+                    fsPlusGit.stat(Path.resolve(pathToTestRepo, 'gitFakeFs', 'HEAD', 'subdir'), passError(done, function (stats) {
                         expect(stats.isDirectory(), 'to be', true);
                         expect(stats.isFile(), 'to be', false);
                         done();
@@ -49,7 +49,7 @@ describe('FsPlusGit', function () {
                 });
 
                 it('should report /gitFakeFs/HEAD/foo.txt as a file', function (done) {
-                    gitWrappedFs.stat(Path.resolve(pathToTestRepo, 'gitFakeFs', 'HEAD', 'foo.txt'), passError(done, function (stats) {
+                    fsPlusGit.stat(Path.resolve(pathToTestRepo, 'gitFakeFs', 'HEAD', 'foo.txt'), passError(done, function (stats) {
                         expect(stats.isDirectory(), 'to be', false);
                         expect(stats.isFile(), 'to be', true);
                         done();
@@ -57,7 +57,7 @@ describe('FsPlusGit', function () {
                 });
 
                 it('should report /gitFakeFs/branches/ as a directory', function (done) {
-                    gitWrappedFs.stat(Path.resolve(pathToTestRepo, 'gitFakeFs', 'branches'), passError(done, function (stats) {
+                    fsPlusGit.stat(Path.resolve(pathToTestRepo, 'gitFakeFs', 'branches'), passError(done, function (stats) {
                         expect(stats.isDirectory(), 'to be', true);
                         expect(stats.isFile(), 'to be', false);
                         done();
@@ -65,7 +65,7 @@ describe('FsPlusGit', function () {
                 });
 
                 it('should report /gitFakeFs/branches/master/ as a directory', function (done) {
-                    gitWrappedFs.stat(Path.resolve(pathToTestRepo, 'gitFakeFs', 'branches', 'master'), passError(done, function (stats) {
+                    fsPlusGit.stat(Path.resolve(pathToTestRepo, 'gitFakeFs', 'branches', 'master'), passError(done, function (stats) {
                         expect(stats.isDirectory(), 'to be', true);
                         expect(stats.isFile(), 'to be', false);
                         done();
@@ -73,7 +73,7 @@ describe('FsPlusGit', function () {
                 });
 
                 it('should report /gitFakeFs/commits/738876c70f4f5243a6672def4233911678ce38db/ as a directory', function (done) {
-                    gitWrappedFs.stat(Path.resolve(pathToTestRepo, 'gitFakeFs', 'commits', '738876c70f4f5243a6672def4233911678ce38db'), passError(done, function (stats) {
+                    fsPlusGit.stat(Path.resolve(pathToTestRepo, 'gitFakeFs', 'commits', '738876c70f4f5243a6672def4233911678ce38db'), passError(done, function (stats) {
                         expect(stats.isDirectory(), 'to be', true);
                         expect(stats.isFile(), 'to be', false);
                         done();
@@ -81,7 +81,7 @@ describe('FsPlusGit', function () {
                 });
 
                 it('should return an ENOENT error for an unsupported entry in /gitFakeFs/', function (done) {
-                    gitWrappedFs.stat(Path.resolve(pathToTestRepo, 'gitFakeFs', 'foo'), function (err) {
+                    fsPlusGit.stat(Path.resolve(pathToTestRepo, 'gitFakeFs', 'foo'), function (err) {
                         expect(err, 'to be an', Error);
                         done();
                     });
@@ -91,7 +91,7 @@ describe('FsPlusGit', function () {
 
         describe('#readdir()', function () {
             it('should include the /gitFakeFs/ directory when applied to the .git folder', function (done) {
-                gitWrappedFs.readdir(Path.resolve(pathToTestRepo), passError(done, function (entryNames) {
+                fsPlusGit.readdir(Path.resolve(pathToTestRepo), passError(done, function (entryNames) {
                     expect(entryNames, 'to be an array');
                     expect(entryNames, 'to contain', 'gitFakeFs');
                     done();
@@ -99,7 +99,7 @@ describe('FsPlusGit', function () {
             });
 
             it('should return the types of objects when applied to the virtual /gitFakeFs/ directory', function (done) {
-                gitWrappedFs.readdir(Path.resolve(pathToTestRepo, 'gitFakeFs'), passError(done, function (entryNames) {
+                fsPlusGit.readdir(Path.resolve(pathToTestRepo, 'gitFakeFs'), passError(done, function (entryNames) {
                     expect(entryNames, 'to be an array');
                     expect(entryNames, 'to equal', ['HEAD', 'branches', 'tags', 'commits', 'index', 'changesInIndex']);
                     done();
@@ -107,7 +107,7 @@ describe('FsPlusGit', function () {
             });
 
             it('should list the branches when applied to the virtual /gitFakeFs/branches/ directory', function (done) {
-                gitWrappedFs.readdir(Path.resolve(pathToTestRepo, 'gitFakeFs', 'branches'), passError(done, function (entryNames) {
+                fsPlusGit.readdir(Path.resolve(pathToTestRepo, 'gitFakeFs', 'branches'), passError(done, function (entryNames) {
                     expect(entryNames, 'to be an array');
                     expect(entryNames, 'to contain', 'master');
                     done();
@@ -115,7 +115,7 @@ describe('FsPlusGit', function () {
             });
 
             it('should list the tags when applied to the virtual /gitFakeFs/tag/ directory', function (done) {
-                gitWrappedFs.readdir(Path.resolve(pathToTestRepo, 'gitFakeFs', 'tags'), passError(done, function (entryNames) {
+                fsPlusGit.readdir(Path.resolve(pathToTestRepo, 'gitFakeFs', 'tags'), passError(done, function (entryNames) {
                     expect(entryNames, 'to be an array');
                     expect(entryNames, 'to contain', 'myTag');
                     done();
@@ -123,7 +123,7 @@ describe('FsPlusGit', function () {
             });
 
             it('should list the commits when applied to the virtual /gitFakeFs/commits/ directory', function (done) {
-                gitWrappedFs.readdir(Path.resolve(pathToTestRepo, 'gitFakeFs', 'commits'), passError(done, function (entryNames) {
+                fsPlusGit.readdir(Path.resolve(pathToTestRepo, 'gitFakeFs', 'commits'), passError(done, function (entryNames) {
                     expect(entryNames, 'to be an array');
                     expect(entryNames, 'to contain', '91fe03e2a9f37e49ddc0cf1a1fd19ef44d9b7c4b');
                     done();
@@ -131,7 +131,7 @@ describe('FsPlusGit', function () {
             });
 
             it('should list the contents of the index when applied to the virtual /gitFakeFs/index/ directory', function (done) {
-                gitWrappedFs.readdir(Path.resolve(pathToTestRepo, 'gitFakeFs', 'index'), passError(done, function (entryNames) {
+                fsPlusGit.readdir(Path.resolve(pathToTestRepo, 'gitFakeFs', 'index'), passError(done, function (entryNames) {
                     expect(entryNames, 'to be an array');
                     expect(entryNames, 'to contain', 'stagedFile.txt');
                     done();
@@ -139,7 +139,7 @@ describe('FsPlusGit', function () {
             });
 
             it('should list the contents of the index when applied to the virtual /gitFakeFs/index/ directory', function (done) {
-                gitWrappedFs.readdir(Path.resolve(pathToTestRepo, 'gitFakeFs', 'index'), passError(done, function (entryNames) {
+                fsPlusGit.readdir(Path.resolve(pathToTestRepo, 'gitFakeFs', 'index'), passError(done, function (entryNames) {
                     expect(entryNames, 'to be an array');
                     expect(entryNames, 'to contain', 'stagedFile.txt');
                     expect(entryNames, 'to contain', 'foo.txt');
@@ -149,7 +149,7 @@ describe('FsPlusGit', function () {
             });
 
             it('should list the entries that have changed in the index when applied to the virtual /gitFakeFs/changesInIndex/ directory', function (done) {
-                gitWrappedFs.readdir(Path.resolve(pathToTestRepo, 'gitFakeFs', 'changesInIndex'), passError(done, function (entryNames) {
+                fsPlusGit.readdir(Path.resolve(pathToTestRepo, 'gitFakeFs', 'changesInIndex'), passError(done, function (entryNames) {
                     expect(entryNames, 'to be an array');
                     expect(entryNames, 'to contain', 'stagedFile.txt');
                     expect(entryNames, 'not to contain', 'foo.txt');
@@ -159,7 +159,7 @@ describe('FsPlusGit', function () {
             });
 
             it('should work outside the .git repository', function (done) {
-                gitWrappedFs.readdir(__dirname, passError(done, function (entryNames) {
+                fsPlusGit.readdir(__dirname, passError(done, function (entryNames) {
                     expect(entryNames, 'to be an array');
                     expect(entryNames, 'to contain', Path.basename(__filename));
                     done();
@@ -169,7 +169,7 @@ describe('FsPlusGit', function () {
 
         describe('#readFile()', function () {
             it('should proxy a path outside a .git repository to the wrapped fs implementation', function (done) {
-                gitWrappedFs.readFile(__filename, 'utf-8', passError(done, function (gitFakeFs) {
+                fsPlusGit.readFile(__filename, 'utf-8', passError(done, function (gitFakeFs) {
                     expect(fs.readFile, 'was called once');
                     expect(fs.readFile, 'was always called with', __filename, 'utf-8');
                     expect(gitFakeFs, 'to match', /FsPlusGit/);
@@ -178,7 +178,7 @@ describe('FsPlusGit', function () {
             });
 
             it('should proxy a path inside a branch to the GitFakeFs', function (done) {
-                gitWrappedFs.readFile(Path.resolve(pathToTestRepo, 'gitFakeFs', 'branches', 'master', 'foo.txt'), 'utf-8', passError(done, function (gitFakeFs) {
+                fsPlusGit.readFile(Path.resolve(pathToTestRepo, 'gitFakeFs', 'branches', 'master', 'foo.txt'), 'utf-8', passError(done, function (gitFakeFs) {
                     expect(fs.readFile, 'was not called');
                     expect(gitFakeFs, 'to match', /This is the second revision of foo\.txt/);
                     done();
@@ -186,7 +186,7 @@ describe('FsPlusGit', function () {
             });
 
             it('should proxy a path inside a commit to the GitFakeFs', function (done) {
-                gitWrappedFs.readFile(Path.resolve(pathToTestRepo, 'gitFakeFs', 'commits', '738876c70f4f5243a6672def4233911678ce38db', 'foo.txt'), 'utf-8', passError(done, function (gitFakeFs) {
+                fsPlusGit.readFile(Path.resolve(pathToTestRepo, 'gitFakeFs', 'commits', '738876c70f4f5243a6672def4233911678ce38db', 'foo.txt'), 'utf-8', passError(done, function (gitFakeFs) {
                     expect(fs.readFile, 'was not called');
                     expect(gitFakeFs, 'to match', /This is the first revision of foo\.txt/);
                     done();
